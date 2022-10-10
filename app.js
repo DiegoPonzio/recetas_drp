f44c9fecdaf6bd6732fe77c9d7b3de1f7f891c31
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-10-09 20:17:54 -0500

    NodeJS app Recetas

---
+++ app.js
@@ -0,0 +1,160 @@
+const express = require('express');
+const { conforms, result } = require('lodash');
+const lodash = require('lodash')
+const mysql = require('mysql2');
+
+
+const app = express();
+
+let con = mysql.createConnection({
+    host: 'containers-us-west-93.railway.app',
+    user: 'root',
+    database: 'railway',
+    password: 'dxttLUbPaEbvOe1PNDzw',
+    port: '5469'
+});
+
+con.connect(err =>{
+    if(err){
+       console.log(err);
+    }else{
+        console.log('Se conecto <3');
+    } 
+});
+
+app.set('views', './views');
+app.set("view engine", "pug");
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res)=>{
+    res.render('inicio',{
+        title : 'Home' 
+    });
+});
+
+app.get('/Recetario', (req, res)=>{
+    const querry = 'select * from Receta'
+
+    con.query(querry, (err, result)=>{
+        if(err){
+            console.log(err);
+            res.render('plantilla',{
+                title : 'Recetario',
+                recetas: err
+            });
+        } else {
+            res.render('plantilla',{
+                title : 'Recetario',
+                recetas: result
+            });
+        }    
+    })
+
+});
+
+app.get('/Crear-Receta', (req, res) =>{
+    res.render('crearReceta', {
+        title: 'Crear Receta'
+    })
+});
+
+app.post('/Subir-Receta', (req, res) =>{
+    let {nom_rec} = req.body;
+    let {desc_rec} = req.body;
+    let {url_rec} = req.body;
+    const querry = `insert into Receta (rec_nom,rec_desc, rec_img)values ("${nom_rec}", "${desc_rec}", "${url_rec}")`
+    con.query(querry, (err, result) =>{
+        if(err){
+            res.render('negado',{
+                title: 'Negado',
+                desc: result,
+                tipo: 'Crear'
+            })
+        } else {
+            res.render('aceptado',{
+                title: 'Aceptado',
+                desc: result,
+                tipo: 'Crear'
+            })
+        }
+    })
+});
+
+
+app.get('/Eliminar-Receta', (req, res) =>{
+    let {id_rec}  = req.query;
+    const query = `DELETE FROM Receta WHERE rec_id = ${id_rec}`;
+    con.query(query, (err, result) =>{
+        if(err){
+            res.render('negado',{
+                title: 'Negado',
+                desc: result,
+                tipo: 'Eliminar'
+            })
+        } else {
+            res.render('aceptado',{
+                title: 'Aceptado',
+                desc: result,
+                tipo: 'Eliminar'
+            })
+        }
+    })
+})
+
+app.get('/Editar-Receta', (req, res) =>{
+    let {id_rec} = req.query;
+    const query = `select * from Receta where rec_id=${id_rec}`
+    con.query(query, (err, result)=>{
+        if(err){
+            res.render('negado', {
+                title: 'Negado',
+                tipo: 'Valor no encontrado 😿'
+            })
+        } else {
+            if(result.length > 0){
+                result.map(rec =>{
+                    res.render('editarReceta',{
+                        title: 'Editar',
+                        id_rec: rec.rec_id,
+                        nom_rec: rec.rec_nom,
+                        desc_rec: rec.rec_desc,
+                        url_rec: rec.rec_img
+                    })
+                })
+            } else{
+                res.render('negado', {
+                    title: 'Negado',
+                    tipo: 'Valor no encontrado 😿'
+                })
+            }
+        }
+    })
+
+})
+
+app.post('/Editar-Receta', (req, res) =>{
+    let {nom_rec} = req.body;
+    let {desc_rec} = req.body;
+    let {url_rec} = req.body;
+    let {id_rec}  = req.body;
+    const query = `update Receta set rec_nom='${nom_rec}', rec_desc='${desc_rec}', rec_img= '${url_rec}' where rec_id=${id_rec}`;
+    con.query(query, (err, result) =>{
+        if(err){
+            res.render('negado',{
+                title: 'Negado',
+                desc: result,
+                tipo: 'Editar'
+            })
+        } else{
+            res.render('aceptado',{
+                title: 'Aceptado',
+                desc: result,
+                tipo: 'Editar'
+            })
+        }
+    })
+});
+
+app.listen(3000, ()=>{
+    console.log('corriendo en el puerto 3000');
+})
---
+++ recetario.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: recetario
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `receta`
+--
+
+DROP TABLE IF EXISTS `receta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `receta` (
+  `rec_id` int NOT NULL AUTO_INCREMENT,
+  `rec_nom` varchar(80) NOT NULL,
+  `rec_desc` varchar(150) DEFAULT NULL,
+  `rec_img` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`rec_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `receta`
+--
+
+LOCK TABLES `receta` WRITE;
+/*!40000 ALTER TABLE `receta` DISABLE KEYS */;
+INSERT INTO `receta` VALUES (2,'BURRITOS CON QUESO AL HORNO','Burritos al horno con queso gratinado, frijoles y algo mas...','https://images.aws.nestle.recipes/resized/2020_06_23T12_11_19_mrs_ImageRecipes_144308lrg_448_448.jpg'),(5,'Lasaña de carne casera.','Lasaña casera, deliciosa y elaborada de forma tradicional','https://www.recetasderechupete.com/wp-content/uploads/2020/02/Lasa%C3%B1a-de-carne-768x527.jpg');
+/*!40000 ALTER TABLE `receta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-09 19:06:22
